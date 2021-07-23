ab6a6ce10b9bd0e3b2c824b96de74faa368d7057
Vihocencu_G <61006477+vihocencu@users.noreply.github.com>
2021-07-23 20:29:52 +0200

    missing files

---
+++ library-ui/src/app/books-list/book-factory.ts
@@ -0,0 +1,11 @@
+import {Book} from './book'
+export class BookFactory {
+
+    static fromObject(rawObject:any) {
+        return new Book(
+            rawObject.title,
+            rawObject.author,
+            rawObject.description
+        );
+    }
+}
