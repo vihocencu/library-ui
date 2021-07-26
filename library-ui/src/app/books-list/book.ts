c20f4db28a81d829978c0ade864bab1d16a6baf4
Vihocencu_G <61006477+vihocencu@users.noreply.github.com>
2021-07-26 09:29:03 +0200

    Working stand without pictures

---
+++ library-ui/src/app/books-list/book.ts
@@ -0,0 +1,11 @@
+export class Book {
+    title:string;
+    description:string;
+    author:string;
+
+    constructor(title:string, author:string, description:string) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+    }
+  };
